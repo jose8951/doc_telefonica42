53ff049e4e9d62d0cd5c95f3f246b34b7b0cd67b
jose vega <josves222@gmail.com>
2023-10-07 20:09:26 +0200

    split
    split

---
+++ ft_split.c
@@ -0,0 +1,65 @@
+#include "libft.h"
+
+static int	numwords(char const *s, char c)
+{
+	   int i;
+    int trigger;
+
+    i = 0;
+    trigger = 0;
+    while (*s)
+    {
+        if (*s != c && trigger == 0)
+        {
+            trigger = 1;
+            i++;
+        }
+        else if (*s == c)
+            trigger = 0;
+        s++;
+    }
+    return (i);
+}
+
+static int	split_words(char **result, char const *s, char c, int word)
+{
+	int		start_cur;
+	int		end_cur;
+
+	end_cur = 0;
+	start_cur = 0;
+	while (s[end_cur])
+	{
+		if (s[end_cur] == c || s[end_cur] == 0)
+			start_cur = end_cur + 1;
+		if (s[end_cur] != c && (s[end_cur + 1] == c || s[end_cur + 1] == 0))
+		{
+			result[word] = malloc(sizeof(char) * (end_cur - start_cur + 2));
+			if (!result[word])
+			{
+				while (word++)
+					free(result[word]);
+				return (0);
+			}
+			ft_strlcpy(result[word], (s + start_cur), end_cur - start_cur + 2);
+			word++;
+		}
+		end_cur++;
+	}
+	result[word] = 0;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+
+	if (!s)
+		return (NULL);
+	result = malloc(sizeof(char *) * (numwords(s, c) + 1));
+	if (!result)
+		return (NULL);
+	if (!split_words(result, s, c, 0))
+		return (NULL);
+	return (result);
+}
